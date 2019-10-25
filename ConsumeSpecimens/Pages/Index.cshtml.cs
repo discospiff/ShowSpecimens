ad2e15cb129146c66e1235e82ca9ca1e3b476968
jonesbr <brandan.jones@uc.edu>
2019-10-24 20:29:46 -0400

    Add project files.

---
+++ ConsumeSpecimens/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using QuickType;
+
+namespace ConsumeSpecimens.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            using (var webClient = new WebClient())
+            {
+                String jsonString = webClient.DownloadString("https://www.plantplaces.com/perl/mobile/viewspecimenlocations.pl?Lat=39.14455075&Lng=-84.5093939666667&Range=0.5&Source=location&Version=2");
+                Welcome welcome = Welcome.FromJson(jsonString);
+                List<Specimen> specimens = welcome.Specimens;
+                ViewData["Specimens"] = specimens;
+                
+            }
+        }
+    }
+}
